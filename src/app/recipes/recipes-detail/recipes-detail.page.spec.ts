d2fd1b99b0c3c32904bcfa887e7533b4ee3ecb5d
Samip Shrestha <javasamip@gmail.com>
2019-12-05 22:14:32 +0545

    Added recipe page

--- src/app/recipes/recipes-detail/recipes-detail.page.spec.ts
+++ src/app/recipes/recipes-detail/recipes-detail.page.spec.ts
@@ -1,19 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
-import { HomePage } from './home.page';
+import { RecipesDetailPage } from './recipes-detail.page';
 
-describe('HomePage', () => {
-  let component: HomePage;
-  let fixture: ComponentFixture<HomePage>;
+describe('RecipesDetailPage', () => {
+  let component: RecipesDetailPage;
+  let fixture: ComponentFixture<RecipesDetailPage>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomePage ],
+      declarations: [ RecipesDetailPage ],
       imports: [IonicModule.forRoot()]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(HomePage);
+    fixture = TestBed.createComponent(RecipesDetailPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
