9a3c48da8b5eafe3e040c18a929abe8d84c2cd70
Samip Shrestha <javasamip@gmail.com>
2020-01-07 00:16:49 +0545

    7.21 Added auth guard

---
+++ src/app/auth/auth.guard.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { CanLoad, Route, UrlSegment, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanLoad {
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
+    const isUserAuthenticated = this.authService.userIsAuthenticated;
+
+    if (!isUserAuthenticated) {
+      this.router.navigateByUrl('/auth');
+    }
+    return isUserAuthenticated;
+  }
+
+}
--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.page.html',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  onLogin() {
+    this.authService.login();
+    this.router.navigateByUrl('/places');
   }
 
 }
