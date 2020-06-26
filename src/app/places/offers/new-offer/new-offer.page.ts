4fd06411735b794c12fec8fd03b07d29d262f72b
Samip Shrestha <samip.shrestha@itonics.de>
2020-06-26 13:59:03 +0545

    10.12: New offer form finalize

--- src/app/places/offers/new-offer/new-offer.page.ts
+++ src/app/places/offers/new-offer/new-offer.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-offer',
@@ -7,12 +8,39 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./new-offer.page.scss'],
 })
 export class NewOfferPage implements OnInit {
+  form: FormGroup;
 
   constructor(
     private navCtrl: NavController
   ) { }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      title: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required]
+      }),
+      description: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.maxLength(180)]
+      }),
+      price: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.min(1)]
+      }),
+      fromDate: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required]
+      }),
+      toDate: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required]
+      })
+    })
+  }
+
+  onCreateOffer(){
+    console.log(this.form);
   }
 
   onAddOffer() {
