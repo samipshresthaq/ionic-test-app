2dc58aa460c04c2c3d237a7fa2972612f12996f6
Samip Shrestha <samip.shrestha@itonics.de>
2020-06-27 23:32:43 +0545

    11.8: Added segement filter

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import {SegmentChangeEventDetail} from '@ionic/core';
 import { Subscription } from 'rxjs';
 
+import { AuthService } from 'src/app/auth/auth.service';
+
 import { PlacesService } from '../places.service';
 import { Place } from '../places.model';
 
@@ -13,20 +15,30 @@ import { Place } from '../places.model';
 export class DiscoverPage implements OnInit, OnDestroy {
   loadedPlaces: Place[];
   listedLoadedPlaces: Place[];
+  filteredPlaces: Place[];
   private placesSubs: Subscription;
 
   constructor(
-    private placesService: PlacesService
+    private placesService: PlacesService,
+    private authService: AuthService
   ) { }
 
   ngOnInit() {
     this.placesSubs = this.placesService.places.subscribe(places => {
       this.loadedPlaces = places;
-      this.listedLoadedPlaces = this.loadedPlaces.slice(1);
+      this.filteredPlaces = this.loadedPlaces;
+      this.listedLoadedPlaces = this.filteredPlaces.slice(1);
     });
   }
 
   onFilterUpdate(event: CustomEvent<SegmentChangeEventDetail>) {
+    if (event.detail.value === 'all' ) {
+      this.filteredPlaces = this.loadedPlaces;
+      this.listedLoadedPlaces = this.filteredPlaces.slice(1);
+    } else {
+      this.filteredPlaces = this.loadedPlaces.filter(place => place.userId !== this.authService.userId);
+      this.listedLoadedPlaces = this.filteredPlaces.slice(1);
+    }
     console.log(event.detail);
   }
 
